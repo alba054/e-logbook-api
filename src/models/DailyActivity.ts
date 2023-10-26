cf6187a3010ca32ef2a05b05e6d2661dec560f1a
alba054 <jralif02@gmail.com>
2023-10-26 12:55:27 +0800

    FIX: filter dailyactivity unit

--- src/models/DailyActivity.ts
+++ src/models/DailyActivity.ts
@@ -44,7 +44,7 @@ export class DailyActivity {
             },
           },
           {
-            unitId: unit,
+            unitId: unit === "" ? undefined : unit,
           },
         ],
       },
